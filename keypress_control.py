053bf6e75bc399e113e968b6dcaaba52e4c44dc6
SuperAnjali <anjaliscus@gmail.com>
2019-01-08 16:24:33 +0800

    keyboard rasberry control

--- keypress_control.py
+++ keypress_control.py
@@ -4,16 +4,20 @@ import termios
 import RPi.GPIO as GPIO
 import time
 
-def reset_pins():
-    GPIO.output(pin_left, 0)
-    GPIO.output(pin_right, 0)
-    GPIO.output(pin_up, 0)
-    GPIO.output(pin_down, 0)
+def setup_pins(pins):
+    for pin in pins:
+        GPIO.setup(pin,GPIO.OUT)
 
-def move_car(pin,duration):
-    GPIO.output(pin,1)
+def reset_pins(pins):
+    for pin in pins:
+        GPIO.output(pin, 0)
+    
+def move_car(pins,duration):
+    for pin in pins:
+        GPIO.output(pin,1)
     time.sleep(duration)
-    GPIO.output(pin,0)
+    for pin in pins:
+        GPIO.output(pin,0)
     
 # =============================================================================
 # Main code
@@ -25,32 +29,31 @@ tty.setraw(sys.stdin)
 
 try:
     GPIO.setmode(GPIO.BOARD)
-    pin_left = 31
-    pin_up = 33
-    pin_down = 35
+    pin_revright = 31
+    pin_revleft = 33
+    pin_left = 35
     pin_right = 37
+    PIN_LIST = [pin_left, pin_right, 
+        pin_revleft, pin_revright]
     
-    GPIO.setup(pin_left,GPIO.OUT)
-    GPIO.setup(pin_right,GPIO.OUT)
-    GPIO.setup(pin_up,GPIO.OUT)
-    GPIO.setup(pin_down,GPIO.OUT)
-    
-    
+    setup_pins(PIN_LIST)
+    reset_pins(PIN_LIST)
     x = 0
-    reset_pins()
     duration = 1
     
     while x != chr(27): # ESC
         x=sys.stdin.read(1)[0]
-        #print("You pressed", x)
+        print(x)
         if x == 'a':
-            move_car(pin_left, duration)
+            move_car([pin_revleft, pin_right], duration)
         elif x == 'd':
-            move_car(pin_right, duration)
+            move_car([pin_left,pin_revright], duration)
         elif x == 'w':
-            move_car(pin_up, duration)
+            move_car([pin_left, pin_right], duration)
         elif x == 's':
-            move_car(pin_down, duration)
+            move_car([pin_revleft, pin_revright], duration)
 finally:        
     # Terminal restore
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)  
+    reset_pins(PIN_LIST)
+    GPIO.cleanup()
